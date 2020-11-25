2012adb22eaf40d91aa18354b1bb6bc00cf3e4ef
Administrator <780570471@qq.com>
2020-11-25 16:56:58 +0800

    通过springcloud bus实现动态刷新数据源

---
+++ cloud-config-client-dadasource3377/src/main/java/com/bsoft/CloudConfigClientDataSource3377.java
@@ -0,0 +1,16 @@
+package com.bsoft;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@MapperScan("com.bsoft.dao")
+@EnableEurekaClient//注解@EnableEurekaClient 表明自己是一个eurekaclient
+public class CloudConfigClientDataSource3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientDataSource3377.class,args);
+    }
+}
